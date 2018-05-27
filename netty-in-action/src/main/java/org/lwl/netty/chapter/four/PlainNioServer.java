e4af8c9431b7713ab5f9e6c178a8de0287180610
thinking_fioa <thinking_fioa@163.com>
2018-05-27 20:46:07 +0800

    java Oio/Nio in chapter04

--- netty-in-action/src/main/java/org/lwl/netty/chapter/four/PlainNioServer.java
+++ netty-in-action/src/main/java/org/lwl/netty/chapter/four/PlainNioServer.java
@@ -1,5 +1,16 @@
 package org.lwl.netty.chapter.four;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * @author thinking_fioa
  * @createTime 2018/5/27
@@ -8,4 +19,60 @@ package org.lwl.netty.chapter.four;
 
 
 public class PlainNioServer {
+    public void serve(int port) throws IOException {
+        ServerSocketChannel serverChannel = ServerSocketChannel.open();
+        serverChannel.configureBlocking(false);
+        ServerSocket ss = serverChannel.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        ss.bind(address);
+        Selector selector = Selector.open();
+        serverChannel.register(selector, SelectionKey.OP_ACCEPT);
+        final ByteBuffer msg = ByteBuffer.wrap("Hi!\r\n".getBytes());
+        for (;;){
+            try {
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                //handle exception
+                break;
+            }
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+            while (iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+                try {
+                    if (key.isAcceptable()) {
+                        ServerSocketChannel server =
+                                (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        client.configureBlocking(false);
+                        client.register(selector, SelectionKey.OP_WRITE |
+                                SelectionKey.OP_READ, msg.duplicate());
+                        System.out.println(
+                                "Accepted connection from " + client);
+                    }
+                    if (key.isWritable()) {
+                        SocketChannel client =
+                                (SocketChannel) key.channel();
+                        ByteBuffer buffer =
+                                (ByteBuffer) key.attachment();
+                        while (buffer.hasRemaining()) {
+                            if (client.write(buffer) == 0) {
+                                break;
+                            }
+                        }
+                        client.close();
+                    }
+                } catch (IOException ex) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException cex) {
+                        // ignore on close
+                    }
+                }
+            }
+        }
+    }
 }
--- netty-in-action/src/main/java/org/lwl/netty/chapter/four/PlainOioServer.java
+++ netty-in-action/src/main/java/org/lwl/netty/chapter/four/PlainOioServer.java
@@ -1,5 +1,11 @@
 package org.lwl.netty.chapter.four;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
+
 /**
  * @author thinking_fioa
  * @createTime 2018/5/27
@@ -8,4 +14,37 @@ package org.lwl.netty.chapter.four;
 
 
 public class PlainOioServer {
+    public void serve(int port) throws IOException {
+        final ServerSocket socket = new ServerSocket(port);
+        try {
+            for(;;) {
+                final Socket clientSocket = socket.accept();
+                System.out.println(
+                        "Accepted connection from " + clientSocket);
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        OutputStream out;
+                        try {
+                            out = clientSocket.getOutputStream();
+                            out.write("Hi!\r\n".getBytes(
+                                    Charset.forName("UTF-8")));
+                            out.flush();
+                            clientSocket.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } finally {
+                            try {
+                                clientSocket.close();
+                            } catch (IOException ex) {
+                                // ignore on close
+                            }
+                        }
+                    }
+                }).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
